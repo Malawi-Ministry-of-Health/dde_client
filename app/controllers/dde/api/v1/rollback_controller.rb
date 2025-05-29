2de9fe2ba3486be5e847d93c7b7f25f5f30f9d16
fchiyenda <fchiyenda>
2025-05-29 12:01:59 +0200

    Updated for production

--- app/controllers/dde/api/v1/rollback_controller.rb
+++ app/controllers/dde/api/v1/rollback_controller.rb
@@ -1,25 +1,32 @@
 # frozen_string_literal: true
 
 # controller for managing merge rollback
-class Api::V1::RollbackController < ApplicationController
-  def merge_history
-    identifier = params.require(:identifier)
-    render json: merge_service.get_patient_audit(identifier), status: :ok
-  end
-
-  def rollback_patient
-    patient_id = params.require(:patient_id)
-    visit_type_id = params.require(:visit_type_id)
-    render json: rollback_service.rollback_merged_patient(patient_id, visit_type_id), status: :ok
-  end
-
-  private
-
-  def merge_service
-    MergeAuditService.new
-  end
-
-  def rollback_service
-    RollbackService.new
+module Dde
+  module Api
+    module V1
+      class Api::V1::RollbackController < ApplicationController
+        def merge_history
+          identifier = params.require(:identifier)
+          render json: merge_service.get_patient_audit(identifier), status: :ok
+        end
+      
+        def rollback_patient
+          patient_id = params.require(:patient_id)
+          visit_type_id = params.require(:visit_type_id)
+          render json: rollback_service.rollback_merged_patient(patient_id, visit_type_id), status: :ok
+        end
+      
+        private
+      
+        def merge_service
+          MergeAuditService.new
+        end
+      
+        def rollback_service
+          RollbackService.new
+        end
+      end
+    end
   end
 end
+
